ef3a293375d045e68df55499edb35e9edd1cddba
Grant Davis <grant.davis@gmail.com>
2014-01-01 09:47:55 -0700

    add initial set of tests

--- Project/GDIIndexBar/GDIIndexBarTests/GDIIndexBarTests.m
+++ Project/GDIIndexBar/GDIIndexBarTests/GDIIndexBarTests.m
@@ -7,8 +7,11 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <GDIIndexBar/GDIIndexBar.h>
 
-@interface GDIIndexBarTests : XCTestCase
+@interface GDIIndexBarTests : XCTestCase {
+    UITableView *_tableView;
+}
 
 @end
 
@@ -17,18 +20,54 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    _tableView = [[UITableView alloc] initWithFrame:CGRectZero];
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    _tableView = nil;
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testInitReturnsNotNil
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
+    XCTAssertNotNil(indexBar, @"Index bar must not be nil");
+}
+
+- (void)testTableViewEqualsExpectedTableView
+{
+    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
+    XCTAssertTrue([indexBar.tableView isEqual:_tableView], @"Table view must equal the view it was instantiated with.");
+}
+
+- (void)testAppearanceTextColor
+{
+    [[GDIIndexBar appearance] setTextColor:[UIColor redColor]];
+    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
+    XCTAssertTrue([indexBar.textColor isEqual:[UIColor redColor]], @"Text color does not match color set by appearance protocol");
+}
+
+- (void)testAppearanceTextShadowColor
+{
+    [[GDIIndexBar appearance] setTextShadowColor:[UIColor purpleColor]];
+    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
+    XCTAssertTrue([indexBar.textShadowColor isEqual:[UIColor purpleColor]], @"Text shadow color does not match color set by appearance protocol");
+}
+
+- (void)testAppearanceTextFont
+{
+    UIFont *font = [UIFont italicSystemFontOfSize:11.5];
+    [[GDIIndexBar appearance] setTextFont:font];
+    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
+    XCTAssertTrue([indexBar.textFont isEqual:font], @"Text font does not match font set by appearance protocol");
+}
+
+- (void)testAppearanceBarBackgroundColor
+{
+    [[GDIIndexBar appearance] setBarBackgroundColor:[UIColor orangeColor]];
+    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
+    XCTAssertTrue([indexBar.barBackgroundColor isEqual:[UIColor orangeColor]], @"Bar background color does not match color set by appearance protocol");
 }
 
 @end
