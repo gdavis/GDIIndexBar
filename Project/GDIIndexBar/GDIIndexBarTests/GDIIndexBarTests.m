3167f05c3de565c531f33df8ab7fcdf626b440cb
Grant Davis <grant.davis@gmail.com>
2014-01-01 18:42:42 -0700

    add tests for text rect. refactor creation of index bar and delegate to setUp routine

--- Project/GDIIndexBar/GDIIndexBarTests/GDIIndexBarTests.m
+++ Project/GDIIndexBar/GDIIndexBarTests/GDIIndexBarTests.m
@@ -11,18 +11,19 @@
 
 #define kTableRect CGRectMake(0, 64.f, 320.f, 468.f)
 
-@interface GDIIndexBarTests : XCTestCase {
-    UIView *_containerView;
-    UITableView *_tableView;
-}
-
-@end
-
-
 @interface GDIIndexBarMockDelegate : NSObject <GDIIndexBarDelegate>
 @property (strong, nonatomic) NSArray *indexStrings;
 @end
 
+
+@interface GDIIndexBarTests : XCTestCase {
+    UIView *_containerView;
+    UITableView *_tableView;
+    GDIIndexBar *_indexBar;
+    GDIIndexBarMockDelegate *_delegate;
+}
+
+@end
 
 @implementation GDIIndexBarTests
 
@@ -31,12 +32,21 @@
     [super setUp];
     _containerView = [[UIView alloc] initWithFrame:kTableRect];
     _tableView = [[UITableView alloc] initWithFrame:kTableRect];
-    [_containerView addSubview:_tableView];
+    
+    _indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
+    _delegate = [[GDIIndexBarMockDelegate alloc] init];
+    _indexBar.delegate = _delegate;
+    
     [[[UIApplication sharedApplication] keyWindow] addSubview:_containerView];
+    [_containerView addSubview:_tableView];
+    [_containerView addSubview:_indexBar];
+    [_containerView layoutIfNeeded];
 }
 
 - (void)tearDown
 {
+    _delegate = nil;
+    _indexBar = nil;
     _containerView = nil;
     _tableView = nil;
     [super tearDown];
@@ -44,129 +54,171 @@
 
 - (void)testInitReturnsNotNil
 {
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    XCTAssertNotNil(indexBar, @"Index bar must not be nil");
+    XCTAssertNotNil(_indexBar, @"Index bar must not be nil");
 }
 
 - (void)testTableViewEqualsExpectedTableView
 {
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    XCTAssertTrue([indexBar.tableView isEqual:_tableView], @"Table view must equal the view it was instantiated with.");
+    XCTAssertTrue([_indexBar.tableView isEqual:_tableView], @"Table view must equal the view it was instantiated with.");
 }
 
 - (void)testDelegateEqualToMockDelegate
 {
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    GDIIndexBarMockDelegate *delegate = [[GDIIndexBarMockDelegate alloc] init];
-    indexBar.delegate = delegate;
-    XCTAssertNotNil(indexBar.delegate, @"Delegate should not be nil");
-    XCTAssertEqual(indexBar.delegate, delegate, @"Delegate should be equal to the mock delegate");
+    XCTAssertNotNil(_indexBar.delegate, @"Delegate should not be nil");
+    XCTAssertEqual(_indexBar.delegate, _delegate, @"Delegate should be equal to the mock delegate");
 }
 
 - (void)testVerticalAlignment
 {
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.verticalAlignment = GDIIndexBarAlignmentTop;
-    XCTAssertEqual(indexBar.verticalAlignment, GDIIndexBarAlignmentTop, @"Vertical alignemnt does not equal top alignment.");
-    indexBar.verticalAlignment = GDIIndexBarAlignmentCenter;
-    XCTAssertEqual(indexBar.verticalAlignment, GDIIndexBarAlignmentCenter, @"Vertical alignemnt does not equal center alignment.");
-    indexBar.verticalAlignment = GDIIndexBarAlignmentBottom;
-    XCTAssertEqual(indexBar.verticalAlignment, GDIIndexBarAlignmentBottom, @"Vertical alignemnt does not equal bottom alignment.");
+    _indexBar.verticalAlignment = GDIIndexBarAlignmentTop;
+    XCTAssertEqual(_indexBar.verticalAlignment, GDIIndexBarAlignmentTop, @"Vertical alignemnt does not equal top alignment.");
+    _indexBar.verticalAlignment = GDIIndexBarAlignmentCenter;
+    XCTAssertEqual(_indexBar.verticalAlignment, GDIIndexBarAlignmentCenter, @"Vertical alignemnt does not equal center alignment.");
+    _indexBar.verticalAlignment = GDIIndexBarAlignmentBottom;
+    XCTAssertEqual(_indexBar.verticalAlignment, GDIIndexBarAlignmentBottom, @"Vertical alignemnt does not equal bottom alignment.");
 }
 
 - (void)testAlwaysShowBarBackgroundProperty
 {
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.alwaysShowBarBackground = @(YES);
-    XCTAssertTrue([indexBar.alwaysShowBarBackground isEqualToNumber:@(YES)], @"Always show bar background property does not equal YES");
-    indexBar.alwaysShowBarBackground = @(NO);
-    XCTAssertTrue([indexBar.alwaysShowBarBackground isEqualToNumber:@(NO)], @"Always show bar background property does not equal NO");
+    _indexBar.alwaysShowBarBackground = @(YES);
+    XCTAssertTrue([_indexBar.alwaysShowBarBackground isEqualToNumber:@(YES)], @"Always show bar background property does not equal YES");
+    _indexBar.alwaysShowBarBackground = @(NO);
+    XCTAssertTrue([_indexBar.alwaysShowBarBackground isEqualToNumber:@(NO)], @"Always show bar background property does not equal NO");
 }
 
 - (void)testTextFont
 {
     UIFont *font = [UIFont fontWithName:@"Futura-Medium" size:10.25f];
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.textFont = font;
-    XCTAssertTrue([font isEqual:indexBar.textFont], @"textFont does not match the expected font.");
+    _indexBar.textFont = font;
+    XCTAssertTrue([font isEqual:_indexBar.textFont], @"textFont does not match the expected font.");
 }
 
 - (void)testTextColor
 {
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.textColor = [UIColor orangeColor];
-    XCTAssertTrue([indexBar.textColor isEqual:[UIColor orangeColor]], @"textColor does not match the expected color.");
+    _indexBar.textColor = [UIColor orangeColor];
+    XCTAssertTrue([_indexBar.textColor isEqual:[UIColor orangeColor]], @"textColor does not match the expected color.");
 }
 
 - (void)testTextSpacing
 {
     CGFloat spacing = 17.23f;
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.textSpacing = spacing;
-    XCTAssertTrue(spacing == indexBar.textSpacing, @"textSpacing does not match the expected value.");
+    _indexBar.textSpacing = spacing;
+    XCTAssertTrue(spacing == _indexBar.textSpacing, @"textSpacing does not match the expected value.");
 }
 
 - (void)testTextShadowColor
 {
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.textShadowColor = [UIColor orangeColor];
-    XCTAssertTrue([indexBar.textShadowColor isEqual:[UIColor orangeColor]], @"textShadowColor does not match the expected color.");
+    _indexBar.textShadowColor = [UIColor orangeColor];
+    XCTAssertTrue([_indexBar.textShadowColor isEqual:[UIColor orangeColor]], @"textShadowColor does not match the expected color.");
 }
 
 - (void)testTextShadowOffset
 {
     UIOffset offset = UIOffsetMake(129.34, 34.3);
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.textShadowOffset = offset;
-    XCTAssertTrue(UIOffsetEqualToOffset(offset, indexBar.textShadowOffset), @"textShadowOffset does not match the expected offset.");
+    _indexBar.textShadowOffset = offset;
+    XCTAssertTrue(UIOffsetEqualToOffset(offset, _indexBar.textShadowOffset), @"textShadowOffset does not match the expected offset.");
 }
 
 - (void)testBarBackgroundWidth
 {
     CGFloat width = 28.12;
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.barBackgroundWidth = width;
-    XCTAssertTrue(width == indexBar.barBackgroundWidth, @"barBackgroundWidth does not match the expected value.");
+    _indexBar.barBackgroundWidth = width;
+    XCTAssertTrue(width == _indexBar.barBackgroundWidth, @"barBackgroundWidth does not match the expected value.");
 }
 
 - (void)testBarBackgroundColor
 {
     UIColor *color = [UIColor yellowColor];
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.barBackgroundColor = color;
-    XCTAssertTrue([color isEqual:indexBar.barBackgroundColor], @"barBackgroundColor does not match the expected color.");
+    _indexBar.barBackgroundColor = color;
+    XCTAssertTrue([color isEqual:_indexBar.barBackgroundColor], @"barBackgroundColor does not match the expected color.");
 }
 
 - (void)testBarBackgroundOffset
 {
     UIOffset offset = UIOffsetMake(129.34, 34.3);
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.barBackgroundOffset = offset;
-    XCTAssertTrue(UIOffsetEqualToOffset(offset, indexBar.barBackgroundOffset), @"barBackgroundOffset does not match the expected offset.");
+    _indexBar.barBackgroundOffset = offset;
+    XCTAssertTrue(UIOffsetEqualToOffset(offset, _indexBar.barBackgroundOffset), @"barBackgroundOffset does not match the expected offset.");
 }
 
 - (void)testBarBackgroundCornerRadius
 {
     CGFloat width = 12.39;
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    indexBar.barBackgroundCornerRadius = width;
-    XCTAssertTrue(width == indexBar.barBackgroundCornerRadius, @"barBackgroundCornerRadius does not match the expected value.");
+    _indexBar.barBackgroundCornerRadius = width;
+    XCTAssertTrue(width == _indexBar.barBackgroundCornerRadius, @"barBackgroundCornerRadius does not match the expected value.");
 }
 
 - (void)testIndexBarPositionsOnTheRightSideOfTheTableView
 {
     CGFloat barWidth = [[GDIIndexBar appearance] barWidth];
-    CGRect expectedRect = CGRectMake(_tableView.frame.size.width - barWidth,
+    CGRect expectedRect = CGRectMake(_tableView.frame.origin.x + _tableView.frame.size.width - barWidth,
                                      _tableView.frame.origin.y,
                                      barWidth,
                                      _tableView.frame.size.height);
-    GDIIndexBar *indexBar = [[GDIIndexBar alloc] initWithTableView:_tableView];
-    GDIIndexBarMockDelegate *delegate = [[GDIIndexBarMockDelegate alloc] init];
-    indexBar.delegate = delegate;
-    [_containerView addSubview:indexBar];
-    [_containerView layoutIfNeeded]; // force layout
-    XCTAssertTrue(CGRectEqualToRect(expectedRect, indexBar.frame), @"index bar frame %@ does not match expected rect: %@.",
-                  NSStringFromCGRect(indexBar.frame),
+    XCTAssertTrue(CGRectEqualToRect(expectedRect, _indexBar.frame), @"index bar frame %@ does not match expected rect: %@.",
+                  NSStringFromCGRect(_indexBar.frame),
+                  NSStringFromCGRect(expectedRect));
+}
+
+- (void)testRectForTextAreaWithCenterAlignment
+{
+    _indexBar.verticalAlignment = GDIIndexBarAlignmentCenter;
+    [_indexBar layoutIfNeeded]; // force layout
+    
+    CGFloat barWidth = [[GDIIndexBar appearance] barWidth];
+    UIFont *font = [[GDIIndexBar appearance] textFont];
+    CGFloat lineHeight = [@"0" sizeWithFont:font].height + _indexBar.textSpacing;
+    CGFloat heightOfTexts = [_delegate numberOfIndexesForIndexBar:_indexBar] * lineHeight;
+    CGFloat height = heightOfTexts + _indexBar.textSpacing * 2;
+    CGFloat yp = _tableView.frame.size.height * .5 - height * .5;
+    CGRect expectedRect = CGRectMake(0,
+                                     yp,
+                                     barWidth,
+                                     height);
+    CGRect textRect = [_indexBar rectForTextArea];
+    XCTAssertTrue(CGRectEqualToRect(expectedRect, textRect), @"text rect %@ does not match expected rect: %@.",
+                  NSStringFromCGRect(textRect),
+                  NSStringFromCGRect(expectedRect));
+}
+
+- (void)testRectForTextAreaWithTopAlignment
+{
+    _indexBar.verticalAlignment = GDIIndexBarAlignmentTop;
+    [_indexBar layoutIfNeeded]; // force layout
+    
+    CGFloat barWidth = [[GDIIndexBar appearance] barWidth];
+    UIFont *font = [[GDIIndexBar appearance] textFont];
+    CGFloat lineHeight = [@"0" sizeWithFont:font].height + _indexBar.textSpacing;
+    CGFloat heightOfTexts = [_delegate numberOfIndexesForIndexBar:_indexBar] * lineHeight;
+    CGFloat height = heightOfTexts + _indexBar.textSpacing * 2;
+    CGFloat yp = 0;
+    CGRect expectedRect = CGRectMake(0,
+                                     yp,
+                                     barWidth,
+                                     height);
+    CGRect textRect = [_indexBar rectForTextArea];
+    XCTAssertTrue(CGRectEqualToRect(expectedRect, textRect), @"text rect %@ does not match expected rect: %@.",
+                  NSStringFromCGRect(textRect),
+                  NSStringFromCGRect(expectedRect));
+}
+
+- (void)testRectForTextAreaWithBottomAlignment
+{
+    _indexBar.verticalAlignment = GDIIndexBarAlignmentBottom;
+    [_indexBar layoutIfNeeded]; // force layout
+    
+    CGFloat barWidth = [[GDIIndexBar appearance] barWidth];
+    UIFont *font = [[GDIIndexBar appearance] textFont];
+    CGFloat lineHeight = [@"0" sizeWithFont:font].height + _indexBar.textSpacing;
+    CGFloat heightOfTexts = [_delegate numberOfIndexesForIndexBar:_indexBar] * lineHeight;
+    CGFloat height = heightOfTexts + _indexBar.textSpacing * 2;
+    CGFloat yp = _tableView.frame.size.height - height;
+    CGRect expectedRect = CGRectMake(0,
+                                     yp,
+                                     barWidth,
+                                     height);
+    CGRect textRect = [_indexBar rectForTextArea];
+    XCTAssertTrue(CGRectEqualToRect(expectedRect, textRect), @"text rect %@ does not match expected rect: %@.",
+                  NSStringFromCGRect(textRect),
                   NSStringFromCGRect(expectedRect));
 }
 
